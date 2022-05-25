6827e30edf64ea0b703c055a171ae4c3e50865fc
denis <funiusa@gmail.com>
2022-05-25 13:33:11 +0300

    add redis

--- cmd.sh
+++ cmd.sh
@@ -4,10 +4,10 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
 	echo "Running Development Server" # start server for developer
-	exec python "identidock.py"
+	exec python "identicon.py"
 else
 	echo "Running Production Server" # Start server for operation
-	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identicon.py \
 		--callable app --stats 0.0.0.0:9191
 fi
 
